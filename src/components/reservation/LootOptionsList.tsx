991a62bf816ca1ea626ac83de814ef5494ae74b4
Petter Magnusson <petter.a.magnusson@gmail.com>
2022-05-02 22:13:18 +0200

    * Hide Thori'dal * Show grouped items when making reservation list

--- src/components/reservation/LootOptionsList.tsx
+++ src/components/reservation/LootOptionsList.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { getInstanceData } from '../../api';
 import { getBosses } from '../../api/loot';
-import { Instance, Player } from '../../types';
+import { Boss, Instance, Item, Player } from '../../types';
 import LootOption from './LootOption';
 import style from './LootOptionsList.module.css';
 
 interface LootOptionsListProps {
     instance: Instance,
     loginPlayer?: Player,
+}
+
+const dropsIncludingGrouped = (boss: Boss): Item[] => {
+    const items: Item[] = [];
+    boss.drops.forEach((drop) => {
+        items.push(drop.item);
+        drop.groupedItems.forEach(item => items.push(item));
+    });
+    items.sort((a, b) => a.name.localeCompare(b.name));
+    return items;
 }
 
 const LootOptionsList: React.FunctionComponent<LootOptionsListProps> = ({ instance, loginPlayer }) => {
@@ -24,8 +34,8 @@ const LootOptionsList: React.FunctionComponent<LootOptionsListProps> = ({ instan
                     <div className={style.boss} key={boss.index}>
                         <h4>{boss.name}</h4>
                         <div className={style.bossItems}>
-                            {boss.drops.filter(drop => !drop.item.hidden || loginPlayer).map(drop => (
-                                <LootOption item={drop.item} key={drop.item.id}/>
+                            {dropsIncludingGrouped(boss).filter(item => !item.hidden || loginPlayer).map(item => (
+                                <LootOption item={item} key={item.id}/>
                             ))}
                         </div>
                     </div>
